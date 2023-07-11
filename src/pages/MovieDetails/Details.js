a2f0a4b8f969636ffd9be10aeddee42f99f572ca
Adelina515 <adelgodlevska@gmail.com>
2023-07-12 00:28:15 +0300

    add coment

--- src/pages/MovieDetails/Details.js
+++ src/pages/MovieDetails/Details.js
@@ -4,26 +4,26 @@ import { fetchMoviesDetails } from 'services/ApiAxios';
 
 const Details = () => {
   const [movieId] = useParams();
-  const [loading, setLoading] = useState(false);
-  const [moveDetails, setMoveDetails] = useState(null);
-  const location = useLocation();
+  // const [loading, setLoading] = useState(false);
+  // const [moveDetails, setMoveDetails] = useState(null);
+  // const location = useLocation();
 
-  useEffect(() => {
-    const fetchDetails = () => {
-      setLoading(true);
-      fetchMoviesDetails(movieId)
-        .then(movie => {
-          setMoveDetails(movie);
-        })
-        .catch(error => {
-          console.log(error);
-        })
-        .finally(() => {
-          setLoading(false);
-        });
-    };
+  // useEffect(() => {
+  //   const fetchDetails = () => {
+  //     setLoading(true);
+  //     fetchMoviesDetails(movieId)
+  //       .then(movie => {
+  //         setMoveDetails(movie);
+  //       })
+  //       .catch(error => {
+  //         console.log(error);
+  //       })
+  //       .finally(() => {
+  //         setLoading(false);
+  //       });
+  //   };
 
-    fetchDetails();
-  }, [movieId]);
+  //   fetchDetails();
+  // }, [movieId]);
 };
 export default Details;
