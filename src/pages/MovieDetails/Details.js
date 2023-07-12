5dd7a6018dcc51f1acb85bc2ec4882ac4bf49493
Adelina515 <adelgodlevska@gmail.com>
2023-07-12 09:40:03 +0300

    update details

--- src/pages/MovieDetails/Details.js
+++ src/pages/MovieDetails/Details.js
@@ -1,29 +1,77 @@
+import Loader from 'components/loader/Loader';
 import { useEffect, useState } from 'react';
-import { useLocation, useParams } from 'react-router-dom';
+import { Outlet, useLocation, useParams } from 'react-router-dom';
 import { fetchMoviesDetails } from 'services/ApiAxios';
 
 const Details = () => {
   const [movieId] = useParams();
-  // const [loading, setLoading] = useState(false);
-  // const [moveDetails, setMoveDetails] = useState(null);
-  // const location = useLocation();
+  const [loading, setLoading] = useState(false);
+  const [moveDetails, setMoveDetails] = useState(null);
+  const location = useLocation();
 
-  // useEffect(() => {
-  //   const fetchDetails = () => {
-  //     setLoading(true);
-  //     fetchMoviesDetails(movieId)
-  //       .then(movie => {
-  //         setMoveDetails(movie);
-  //       })
-  //       .catch(error => {
-  //         console.log(error);
-  //       })
-  //       .finally(() => {
-  //         setLoading(false);
-  //       });
-  //   };
+  useEffect(() => {
+    const fetchDetails = () => {
+      setLoading(true);
+      fetchMoviesDetails(movieId)
+        .then(movie => {
+          setMoveDetails(movie);
+        })
+        .catch(error => {
+          console.log(error);
+        })
+        .finally(() => {
+          setLoading(false);
+        });
+    };
 
-  //   fetchDetails();
-  // }, [movieId]);
+    fetchDetails();
+  }, [movieId]);
+
+  if (!moveDetails) {
+    return;
+  }
+  const {
+    original_title,
+    release_date,
+    popularity,
+    overview,
+    poster_path,
+    genre_ids,
+    title,
+  } = moveDetails;
+
+  return (
+    <>
+      <link to={location.state?.from ?? '/'}>
+        <button type="button">Go back</button>
+      </link>
+      {loading && <Loader />}
+      {moveDetails && (
+        <div>
+          <img width="300px" alt="name"></img>
+          <div>
+            <h1>name</h1>
+            <p>User score:</p>
+            <h2>Overview</h2>
+            <p>text</p>
+            <h2>Genres</h2>
+            <p>text</p>
+          </div>
+          <div>
+            <h3>Additional information</h3>
+            <ul>
+              <li>
+                <link to="cast">Cast</link>
+              </li>
+              <li>
+                <link to="reviews">Reviews</link>
+              </li>
+            </ul>
+            <Outlet />
+          </div>
+        </div>
+      )}
+    </>
+  );
 };
 export default Details;
